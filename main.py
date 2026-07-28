5a886fa8629092f9a91481b5a7c41b38c109db46
Adwaith Suvish <adwaithsuvi@gmail.com>
2026-07-29 00:53:39 +0400

    Added support for UDP and fixed bug in file writing

--- main.py
+++ main.py
@@ -2,30 +2,70 @@ import socket
 from concurrent.futures import ThreadPoolExecutor
 
 
-ip = input("Enter the IP address or domain name: ")
-if not ip[0:4].isnumeric():
-    ip = socket.gethostbyname(ip)
-f =  open (f"{ip}_ports.txt", "w")
 
-output = []
 
-def ports_checker(port):
+def ports_checker_TCP(port):
     
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.settimeout(20)
+    s.settimeout(2)
         
     try:
         s.connect((ip,port))
-        output.append(f"Port {port} is open\n")
+        output.append(f"Port {port} is open(TCP)\n")
 
     except (socket.timeout, ConnectionRefusedError, OSError):
-        print(f"Port {port} is closed")
+        print(f"Port {port} is closed(TCP)")
 
     finally:
         s.close()
 
-with ThreadPoolExecutor(max_workers=500) as executor:
-    executor.map(ports_checker, range(1, 65536))
+def ports_checker_UDP(port):
+    
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.settimeout(2)
+        
+    try:
+        s.connect((ip,port))
+        s.send(b'')
+        data,addr = s.recvfrom(1024)
+        output.append(f"Port {port} is open(UDP)\n")
+
+    except socket.timeout:
+        print(f"Port {port} no response(UDP)\n")
+
+    except ConnectionResetError:
+         print(f"Port {port}: closed(UDP)\n")
+
+    finally:
+        s.close()
+
+choice = input("Enter protocol (TCP/UDP OR BOTH)")
+ip = input("Enter the IP address or domain name: ")
+if not ip[0:4].isnumeric():
+    ip = socket.gethostbyname(ip)
+f =  open (f"{ip}_ports.txt", "w")
+output = []
+
+
+if choice.upper() == "TCP":
+    with ThreadPoolExecutor(max_workers=200) as executor:
+        executor.map(ports_checker_TCP, range(1, 65536))
+
+elif choice.upper() == "UDP":
+    with ThreadPoolExecutor(max_workers=200) as executor:
+            executor.map(ports_checker_UDP, range(1, 65536))
+
+elif choice.upper() == "BOTH":
+    with ThreadPoolExecutor(max_workers=200) as executor:
+            executor.map(ports_checker_TCP, range(1, 65536))
+    with ThreadPoolExecutor(max_workers=200) as executor:
+                executor.map(ports_checker_UDP, range(1, 65536))
+
+else:
+     print("Incorrect Choice")
+    
+
+
 
 f.writelines(output)
 f.close()
