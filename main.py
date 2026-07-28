b28c636c81b3a7edabe5e067761698aaa6c766aa
Adwaith Suvish <adwaithsuvi@gmail.com>
2026-07-28 20:44:40 +0400

    Fixed fresh socket per port and Multithreading support

--- main.py
+++ main.py
@@ -1,21 +1,31 @@
 import socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+from concurrent.futures import ThreadPoolExecutor
+
+
 ip = input("Enter the IP address or domain name: ")
 if not ip[0:4].isnumeric():
     ip = socket.gethostbyname(ip)
-
-s.settimeout(30)
 f =  open (f"{ip}_ports.txt", "w")
-for i in range(1,65535):
+
+output = []
+
+def ports_checker(port):
     
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(20)
+        
     try:
-        s.connect((ip,i))
-        
-        f.write(f"Port {i} is open\n")
-        
+        s.connect((ip,port))
+        output.append(f"Port {port} is open\n")
 
     except (socket.timeout, ConnectionRefusedError, OSError):
-        print(f"Port {i} is closed")
+        print(f"Port {port} is closed")
 
-s.close()        
+    finally:
+        s.close()
+
+with ThreadPoolExecutor(max_workers=500) as executor:
+    executor.map(ports_checker, range(1, 65536))
+
+f.writelines(output)
 f.close()
